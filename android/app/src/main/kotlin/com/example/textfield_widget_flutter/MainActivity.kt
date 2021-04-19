37bce6fb6a09c1ef8d4bbd747118fe19eaa20b8e
Refsi Maulana <refsimaulanatkr@gmail.com>
2021-04-19 15:02:04 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/textfield_widget_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.textfield_widget_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Masukkan Password"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                obscureText: true, //untuk membuat text menjadi seperti password
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
+              ),
+              Text(controller.text)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
